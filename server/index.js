d1ea0af355b5491adc16cfa9446a878de998a42d
Ryan Moon <ryanmoondevelopment@gmail.com>
2022-07-19 16:18:04 -0500

    refactored to store routes in a separate file and imported dotenv to enable usage of environmental variables

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
+require("dotenv").config();
 const express = require("express");
 const path = require("path");
 const cluster = require("cluster");
 const numCPUs = require("os").cpus().length;
+const routes = require("./routes");
 
 const isDev = process.env.NODE_ENV !== "production";
 const PORT = process.env.PORT || 5000;
@@ -27,17 +29,7 @@ if (!isDev && cluster.isMaster) {
   app.use(express.static(path.resolve(__dirname, "../react-ui/build")));
 
   // Answer API requests.
-  app.get("/api", (req, res) => {
-    res.set("Content-Type", "application/json");
-    res.send('{"message":"Hello from the custom server!"}');
-  });
-
-  // All remaining requests return the React app, so it can handle routing.
-  app.get("*", (request, response) => {
-    response.sendFile(
-      path.resolve(__dirname, "../react-ui/build", "index.html")
-    );
-  });
+  app.use(routes);
 
   app.listen(PORT, () => {
     console.error(
