48faea2fbe7f10368bd0e185d86403a3e4e7b92f
Ryan Moon <ryanmoondevelopment@gmail.com>
2022-07-20 14:41:15 -0500

    first commit

---
+++ react-ui/src/components/DegreeSymbol/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+interface DegreeSymbolProps {
+  isMetric?:boolean
+  isTemperature:boolean
+}
+
+const DegreeSymbol = ({isMetric=false, isTemperature=false}:DegreeSymbolProps) => {
+
+  if(isTemperature){
+    return <span>&deg;{isMetric?'C':'F'}</span>
+  }
+  return (
+    <span>&deg;</span>
+  )
+}
+
+export default DegreeSymbol
